d2750f91077290fed477137e570785e070473db2
Ubuntu <ubuntu@ip-172-31-29-244.us-west-2.compute.internal>
2013-07-19 20:58:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,29 @@
+var express = require('express');
 var fs = require('fs');
 
-var data = fs.readFileSync('./index.html'; 'utf8);
+var buffer = new Buffer (30);
 
-var app = fs.createServer(fs.logger());
+var app = express.createServer(express.logger());
+
+
+var content = fs.readFileSync("indexx.html");
+
+var data = buffer.toString("utf-8",content);
+     
+      console.log(data);
+
 
 app.get('/', function(request, response) {
-  response.send(data);
+    
+      response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
